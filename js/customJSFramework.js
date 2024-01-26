c2725ce78e67c53345814114aabbfc56d90fbc2b
kubiartwork <kubiartwork@gmail.com>
2024-01-26 12:32:13 -0500

    Update customJSFramework.js

--- js/customJSFramework.js
+++ js/customJSFramework.js
@@ -6,6 +6,16 @@ function getCurrentYear() {
 };
   
 
+// element
+imagesLoaded( document.querySelector('#container'), function( instance ) {
+  console.log('all images are loaded');
+});
+// selector string
+imagesLoaded( '#container', function() {...});
+// multiple elements
+var posts = document.querySelectorAll('.post');
+imagesLoaded( posts, function() {...});
+
 
 
 // vanilla JS
