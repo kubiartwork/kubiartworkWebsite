79132cec3475524656e029c637f9bd8247f2c620
kubiartwork <kubiartwork@gmail.com>
2024-01-26 12:04:39 -0500

    Update customJSFramework.js

--- js/customJSFramework.js
+++ js/customJSFramework.js
@@ -8,10 +8,16 @@ function getCurrentYear() {
 
 
 
-
-
-$('.grid').masonry({
+// vanilla JS
+// init with element
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
   // options...
   itemSelector: '.grid-item',
   columnWidth: 200
+});
+
+// init with selector
+var msnry = new Masonry( '.grid', {
+  // options...
 });
